f5a1a40f8671dfc78f4ade0126c669edcca48262
Jiazhen Xie <fdf515@gmail.com>
2016-11-04 23:21:36 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,60 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005201117) do
+ActiveRecord::Schema.define(version: 20160922173924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "certificates", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "doc_type_id"
-    t.date     "expiry_date"
-    t.integer  "user_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "file_file_name"
-    t.string   "file_content_type"
-    t.integer  "file_file_size"
-    t.datetime "file_updated_at"
-    t.index ["doc_type_id"], name: "index_certificates_on_doc_type_id", using: :btree
-    t.index ["user_id"], name: "index_certificates_on_user_id", using: :btree
-  end
-
-  create_table "doc_types", force: :cascade do |t|
-    t.string   "name",       limit: 80
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.index ["name"], name: "index_doc_types_on_name", using: :btree
-  end
-
-  create_table "tenancies", force: :cascade do |t|
-    t.string   "tenants"
-    t.integer  "term"
-    t.date     "start_date"
-    t.date     "end_date"
-    t.float    "rent"
-    t.float    "deposit"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-    t.index ["user_id"], name: "index_tenancies_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
@@ -71,12 +35,8 @@ ActiveRecord::Schema.define(version: 20161005201117) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "address"
-    t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "certificates", "doc_types"
-  add_foreign_key "certificates", "users"
-  add_foreign_key "tenancies", "users"
 end
