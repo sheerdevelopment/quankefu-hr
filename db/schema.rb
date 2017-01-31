56b581d66707e2b9311cc04492b4b61a8a8c589d
Joe Xie <fdf515@gmail.com>
2017-01-31 15:55:44 +0000

    Correct wrong scope in user

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170116173606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "intarray"
 
   create_table "absence_status_translations", force: :cascade do |t|
     t.integer  "absence_status_id", null: false
