67380aca65c28de89879a8a92cb376a858e6c808
Joe Xie <fdf515@gmail.com>
2017-02-01 22:48:04 +0000

    Customise for Quankefu

--- app/mailers/absence_mailer.rb
+++ app/mailers/absence_mailer.rb
@@ -1,11 +1,11 @@
 class AbsenceMailer < ApplicationMailer
   def request_email(user:, absence:, admin:)
     @user, @absence, @admin = user, absence, admin
-    mail(to: @admin.email, subject: "#{@user.username} has submitted a new absence request.")
+    mail(to: @admin.email, subject: "#{@user.username}提交了新的请假申请。")
   end
 
   def status_email(user:, absence:)
     @user, @absence = user, absence
-    mail(to: @user.email, subject: "Your absence request has been updated.")
+    mail(to: @user.email, subject: "你的假期申请的状态已被批复。")
   end
 end
