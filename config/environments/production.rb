8ccf1c89c474c1f1dd8dac888d6b473cf32489aa
Jiazhen Xie <fdf515@gmail.com>
2017-02-04 10:18:09 +0000

    Update mailer smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,11 +86,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :port           => ENV["MAILGUN_SMTP_PORT"],
-    :address        => ENV["MAILGUN_SMTP_SERVER"],
-    :user_name      => ENV["MAILGUN_SMTP_LOGIN"],
-    :password       => ENV["MAILGUN_SMTP_PASSWORD"],
+    :port           => ENV["MAIL_SMTP_PORT"],
+    :address        => ENV["MAIL_SMTP_SERVER"],
+    :user_name      => ENV["MAIL_SMTP_LOGIN"],
+    :password       => ENV["MAIL_SMTP_PASSWORD"],
     :domain         => ENV["APP_DOMAIN"],
     :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
